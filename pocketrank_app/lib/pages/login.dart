95e5c8295df89c9451d68ef25c87df39e32f59d0
putetrekk <17160920+putetrekk@users.noreply.github.com>
2024-09-13 19:38:16 +0200

    Add horizontal padding to login page

--- pocketrank_app/lib/pages/login.dart
+++ pocketrank_app/lib/pages/login.dart
@@ -68,6 +68,7 @@ class _LoginPageState extends State<LoginPage> {
           constraints: const BoxConstraints(
             maxWidth: 400,
           ),
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
